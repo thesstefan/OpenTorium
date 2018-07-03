a3cecf52f380dfe3e34c8d4a18152db31c44447e
TheMightyStefan <stefanthefineman@gmail.com>
2018-07-03 16:23:22 +0300

    Add multiple particles use case

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -24,10 +24,18 @@ void ofApp::update() {
     if (particles.size() == 0)
         particles.push_back(emitter.createParticle());
 
-    if (!particles[0].isAlive())
-        particles[0] = emitter.createParticle();
+    int index = 0;
+    while (index < particles.size()) {
+        if (particles[index].isAlive() == false) 
+            particles.erase(particles.begin() + index);
+        else
+            index++;
+    }
+    
+    emitter.update(deltaTime, particles);
 
-    particles[0].update(deltaTime);
+    for (int index = 0; index < particles.size(); index++)
+        particles[index].update(deltaTime);
 }
 
 void ofApp::draw() {
@@ -35,7 +43,8 @@ void ofApp::draw() {
 
     fbo.begin();
 
-    particles[0].draw();
+    for (int index = 0; index < particles.size(); index++)
+        particles[index].draw();
 
     fbo.end();
 
