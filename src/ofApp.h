0d27f20c459d0dd284d9b693e4b9993a0147d60f
TheMightyStefan <stefanthefineman@gmail.com>
2018-07-05 15:57:28 +0300

    Documented ofApp

--- src/ofApp.h
+++ src/ofApp.h
@@ -1,17 +1,45 @@
+/**
+ * @file ofApp.h
+ *
+ * @brief This file provides the interface of ofApp.
+ */
+
 #pragma once
 
 #include "ofMain.h"
 
 #include "emitter.h"
 
+/**
+ * @class ofApp
+ *
+ * This class encapsulates the behaviour of the program.
+ *
+ * An Emitter is used to create Particle instances with various
+ * trajectories and speeds which are displayed on the screen.
+ */
 class ofApp : public ofBaseApp {
-    Emitter emitter;
-    std::list<std::unique_ptr<Particle>> particles;
+    private:
+        /** @brief The Emitter used to create Particle instances. **/
+        Emitter emitter;
+        /** @brief The std::list used to store the Particle instances used. **/
+        std::list<std::unique_ptr<Particle>> particles;
 
-    float timePassed;
+        /** @brief The time that has passed since the beginning of the program. **/
+        float timePassed;
 
     public:
+        /** @brief Sets initial properties of the program (e.g. frame rate, background color). **/
         void setup();
+        /** @brief Updates the screen / program.
+         *
+         * The Emitter and every Particle are updated.
+         */
         void update();
+
+        /** @brief Draws the Particle instances.
+         *
+         * draw() is called for every Particle.
+         */
         void draw();
 };
