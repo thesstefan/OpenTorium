dfca3455fe6f3312e83d742fe0a0078e10fcbdc1
TheMightyStefan <stefanthefineman@gmail.com>
2018-07-20 14:34:51 +0300

    Fixed initial Field implementation

--- src/field.cpp
+++ src/field.cpp
@@ -1,19 +1,18 @@
+#include <iostream>
 #include "field.h"
 
 Field::Field(const ofPoint& center, int size) : center(center), size(size) {}
 
 bool pointIsInCircle(const ofPoint& point, const ofPoint& circleCenter, int size) {
-    float distance = (point.x - circleCenter.x) * (point.x - circleCenter.x) +
-                     (point.y - circleCenter.y) * (point.y - circleCenter.y);
+    float distance = ofDistSquared(point.x, point.y, circleCenter.x, circleCenter.y);
 
-    return (distance * distance <= size * size);
+    return distance <= size * size;
 }
 
 void Field::update(std::list<std::unique_ptr<Particle>>& particles) {
-    for (auto it = particles.begin(); it != particles.end(); it++) {
+    for (auto it = particles.begin(); it != particles.end(); it++) 
         if (pointIsInCircle((*it)->getPosition(), center, size))
             (*it)->setColor(ofColor::blue);
-    }
 }
 
 void Field::draw() const{
