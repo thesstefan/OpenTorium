55d438053bfeac5d5f05c699c9be0a529a842c19
TheMightyStefan <stefanthefineman@gmail.com>
2018-08-04 21:13:11 +0300

    Fix map algorithm

--- src/field_map.cpp
+++ src/field_map.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
-
 #include "field_map.h"
+
+#include <iostream>
 
 FieldMap::FieldMap(unsigned int width, unsigned int height) 
     : width(width), height(height), map(height, std::vector<char>(width)) {}
@@ -19,33 +19,35 @@ void FieldMap::update() {
         for (unsigned int width_index = 0; width_index < width; width_index++)
             map[height_index][width_index] = 0;
 
-    for (auto& it : fields)
-        for (unsigned int height_index = 0; height_index < height; height_index++) {
+    int field_index = 0;
+
+    for (auto& it : fields) {
+        for (unsigned int height_index = 0; height_index < height; height_index++)
             for (unsigned int width_index = 0; width_index < width; width_index++) 
                 if (it->getShape().inside(ofPoint(width_index, height_index))) { 
                     if (map[height_index][width_index] == 0)
                         map[height_index][width_index] = 1;
 
-                    map[height_index][width_index] = map[height_index][width_index] << 1;
-
-                    std::cout << (int)map[height_index][width_index] << " ";
+                    map[height_index][width_index] = map[height_index][width_index] << field_index;
                 }
 
-            std::cout << std::endl;
-        }
+        field_index++;
+    }
 }
 
 void FieldMap::updateParticle(std::unique_ptr<Particle>& particle) const {
-    unsigned char id = map[particle->getPosition().y][particle->getPosition().x];
+    if (particle->getPosition().y < height && particle->getPosition().x < width) {
+        unsigned char id = map.at(static_cast<int>(particle->getPosition().y)).at(static_cast<int>(particle->getPosition().x));
 
-    int field_index = fields.size() - 1;
+        int field_index = fields.size() - 1;
 
-    while (id && field_index != -1) {
-        if (id && 1)
-            fields[field_index]->updateParticle(particle);
+        while (id && field_index != -1) {
+            if (id && 1)
+                fields[field_index]->updateParticle(particle);
 
-        id = id >> 1;
+            id = id >> 1;
 
-        field_index--;
+            field_index--;
+        }
     }
 }
